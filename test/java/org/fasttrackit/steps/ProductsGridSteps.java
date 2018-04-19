9fd6a42f7ffd76afec56a19239a3852ef455b024
MarcCristina <crys_20062006@yahoo.com>
2018-04-19 21:14:58 +0300

    Initial commit

---
+++ test/java/org/fasttrackit/steps/ProductsGridSteps.java
@@ -0,0 +1,133 @@
+package org.fasttrackit.steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.fasttrackit.TestBase;
+import org.fasttrackit.webviews.ProductsGrid;
+import org.openqa.selenium.WebElement;
+
+import javax.naming.Name;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ProductsGridSteps extends TestBase {
+
+    private ProductsGrid productsGrid = initElements(ProductsGrid.class);
+
+    @When("^I sort the products by \"([^\"]*)\"$")
+    public void iSortTheProductsBy(String criteria) {
+        productsGrid.getSortByList().selectByVisibleText(criteria);
+    }
+
+    public static void main(String[] args) {
+        // regex demo
+
+        String priceText = "Starting at: 100,00 RON";
+
+        Pattern pattern = Pattern.compile(".* (\\d+,\\d+).*");
+        Matcher matcher = pattern.matcher(priceText);
+
+        Double price = null;
+
+        if (matcher.find()) {
+            String parsablePriceText = matcher.group(1);
+
+            price = Double.parseDouble(parsablePriceText.replace(",", "."));
+        }
+
+        // Use the parsed price in assertions or in any way you need to.
+        System.out.println(price);
+    }
+
+    @And("^I sort the products in (.+) order$")
+    public void iSortTheProductsInDescendingOrder(String sortDirection) {
+        String classValue = productsGrid.getSortDirectionLink().getAttribute("class");
+
+        String expectedPartialClassValue;
+        if (sortDirection.equals("descending")) {
+            expectedPartialClassValue = "--asc";
+        } else {
+            expectedPartialClassValue = "--desc";
+        }
+
+        if (classValue.contains(expectedPartialClassValue)) {
+            productsGrid.getSortDirectionLink().click();
+        }
+    }
+
+    @Then("^products are sorted by \"([^\"]*)\" in (.+) order$")
+    public void productsAreSortedByInDescendingOrder(String criteria, String sortDirection) throws Exception {
+        VerifySortingByPrice(sortDirection);
+        if (criteria.equals("Price")) {
+            VerifySortingByPrice(sortDirection);
+        } else if (criteria.equals("Name")) {
+            VerifySortingByPrice(sortDirection);
+        } else {
+            throw new Exception("Step implemented only for Price and Name criteria");
+        }
+
+    }
+
+
+    private void VerifySortingByPrice(String sortDirection) {
+        List<Double> actualPrices = new ArrayList<>();
+
+        for (WebElement priceContainer : productsGrid.getProductPrices()) {
+            String priceText = priceContainer.getText();
+
+            String priceTextWithoutCurrency = priceText.split(" ")[0];
+
+            priceTextWithoutCurrency = priceTextWithoutCurrency.replace(",", ".");
+
+            double price = Double.parseDouble(priceTextWithoutCurrency);
+
+            actualPrices.add(price);
+        }
+
+        List<Double> sortedPrices = new ArrayList<>(actualPrices);
+
+        Comparator<Double> comparator;
+
+        if (sortDirection.equals("descending")) {
+            comparator = Comparator.reverseOrder();
+        } else {
+            comparator = Comparator.naturalOrder();
+        }
+
+        sortedPrices.sort(comparator);
+
+        assertThat("Products are not sorted correctly.", actualPrices, equalTo(sortedPrices));
+    }
+
+    private void VerifySortingByName(String sortDirection) {
+        List<String> actualNames = new ArrayList<>();
+
+        for (WebElement nameContainer : productsGrid.getProductNames()) {
+            String name = nameContainer.getText();
+
+            actualNames.add(name);
+        }
+
+        List<String> sortedNames = new ArrayList<>(actualNames);
+
+        Comparator<String> comparator;
+
+        if (sortDirection.equals("descending")) {
+            comparator = Comparator.reverseOrder();
+        } else {
+            comparator = Comparator.naturalOrder();
+        }
+
+        sortedNames.sort(comparator);
+
+        assertThat("Products are not sorted correctly.", actualNames, equalTo(sortedNames));
+    }
+}
---
+++ test/resources/features/products-sorting.feature
@@ -0,0 +1,16 @@
+Feature: Products Sorting
+  As a customer
+  I want to sort products by different criteria
+  So that I can easily find what I need
+
+  Scenario Outline: Sort by price
+    Given I open the homepage
+    And I search a product by "<keyword>"
+    When I sort the products by "<sort criteria>"
+    And I sort the products in <sort direction> order
+    Then products are sorted by "<sort criteria>" in <sort direction> order
+
+    Examples:
+      | keyword | sort criteria | sort direction |
+      | vase    | Price         | descending     |
+      | shirt   | Name          | ascending      |
